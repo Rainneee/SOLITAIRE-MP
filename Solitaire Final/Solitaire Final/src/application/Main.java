e5603dea680523fdde2143e8d724df40d83ace5c
yobiboy <76948907+yobiboy@users.noreply.github.com>
2021-01-18 13:12:53 +0800

    Edited Main
    added classes openGame and openMainMenu

--- Solitaire Final/Solitaire Final/src/application/Main.java
+++ Solitaire Final/Solitaire Final/src/application/Main.java
@@ -2,31 +2,65 @@ package application;
 	
 import java.util.Stack;
 
+import application.controller.MainGameController;
+import application.controller.MainMenuController;
 import application.model.*;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.AnchorPane;
 
 
 public class Main extends Application {
+	private Stage primaryStage;
 	@Override
 	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		openMainMenu();
+	}
+	
+	private void openMainMenu() {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/MainMenu.fxml"));
+			AnchorPane mainMenuFXML = (AnchorPane) loader.load();
+
+			Scene scene = new Scene(mainMenuFXML);
+			primaryStage.setTitle("Solitaire");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			MainMenuController menuController = loader.getController();
+			menuController.setMain(this);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void openGame() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/MainGame.fxml"));
+			AnchorPane mainGameFXML = (AnchorPane) loader.load();
+
+			Scene scene = new Scene(mainGameFXML);
+			primaryStage.setTitle("Solitaire");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			MainGameController mainGameController = loader.getController();
+			mainGameController.setMain(this);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
 	public static void main(String[] args) {
-		//launch(args);
-		Game game = new Game();
-		System.out.println(game);
-		System.out.flush();
+		launch(args);
+
 	}
+	
 }
