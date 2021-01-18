f2f4b1af1cbc03742cb631fdefeac9ba7ea435d8
Romaine Dara Regala <rmregala@up.edu.ph>
2021-01-18 15:32:42 +0800

    added gameover

--- Solitaire Final/Solitaire Final/src/application/controller/GameOverController.java
+++ Solitaire Final/Solitaire Final/src/application/controller/GameOverController.java
@@ -1,5 +1,25 @@
 package application.controller;
 
-public class GameOverController {
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
+public class GameOverController {
+	@FXML 
+	private Label playerName, score;
+	private Main main;
+	
+	public void message(Main main) {
+		this.main = main;
+		
+		//playerName.setText(value); value = initiated player sa main
+		//score.setText(""+ valueofscore) value of score = value record sa main;
+		//main.  tagarecord ng score from main
+		
+	}
+	
+	@FXML
+	public void goToMainMenu() {
+		main.openMainMenu();
+	}
 }
