fafd2de19b1c33160a5ac18f77c13bbf9e8f29bc
Romaine Dara Regala <rmregala@up.edu.ph>
2021-01-18 18:43:46 +0800

    Update MainGameController.java

--- Solitaire Final/Solitaire Final/src/application/controller/MainGameController.java
+++ Solitaire Final/Solitaire Final/src/application/controller/MainGameController.java
@@ -2,12 +2,24 @@ package application.controller;
 
 import application.Main;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class MainGameController {
 
 	@FXML
 	private Label playerName, scoreSheet;
+	
+	@FXML
+	private AnchorPane stock, waste, foundation1, foundation2, foundation3, foundation4;
+	
+	@FXML
+	private StackPane tableau1, tableau2, tableau3, tableau4, tableau5, tableau6, tableau7;
+	
+	@FXML
+	private Button recycleWaste;
 	
 	private Main main;
 
