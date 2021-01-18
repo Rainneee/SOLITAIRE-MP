66a30ccc9d8626bcb16652820f5563a50ddfdd4f
yobiboy <76948907+yobiboy@users.noreply.github.com>
2021-01-18 16:42:55 +0800

    Initialized tableau, waste, stock, playername, score

--- Solitaire Final/Solitaire Final/src/application/controller/MainGameController.java
+++ Solitaire Final/Solitaire Final/src/application/controller/MainGameController.java
@@ -1,14 +1,35 @@
 package application.controller;
 
 import application.Main;
+import application.model.Game;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class MainGameController {
 
 	private Main main;
-
-	public void setMain(Main main) {
+	
+	public void setMain(Main main){
 		this.main = main;
-		
 	}
-
+	
+	@FXML
+	private AnchorPane stock, waste, foundation1, foundation2, foundation3, foundation4;
+	
+	@FXML
+	private StackPane tableau1, tableau2, tableau3, tableau4, tableau5, tableau6, tableau7;
+	
+	@FXML
+	private Button recycleWaste;
+	
+	@FXML
+	private Label playerName, currentScore;
+	
+	
+	
+	
 }
