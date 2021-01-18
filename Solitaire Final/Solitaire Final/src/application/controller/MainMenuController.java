aeb17b5ca4bc2948960e98cf23b95d2df9fb7fd3
Romaine Dara Regala <rmregala@up.edu.ph>
2021-01-18 13:05:28 +0800

    Edited MainMenu

--- Solitaire Final/Solitaire Final/src/application/controller/MainMenuController.java
+++ Solitaire Final/Solitaire Final/src/application/controller/MainMenuController.java
@@ -1,5 +1,27 @@
 package application.controller;
 
+import application.Main;
+import javafx.fxml.FXML;
+
 public class MainMenuController {
+	private Main main;
+	
+	public void setMain(Main main) {
+		this.main = main;
+	}
+	
+	@FXML
+	public void startGame(){
+		//main.
+	}
+	
+	@FXML
+	public void seeHighScores(){
+		//main.
+	}
+	
+	public void exit() {
+		//main.
+	}
 
 }
