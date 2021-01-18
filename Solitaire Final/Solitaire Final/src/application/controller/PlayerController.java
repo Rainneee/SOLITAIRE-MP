041572435dd6cc866440e75dd6d1fe3718e686b2
Romaine Dara Regala <rmregala@up.edu.ph>
2021-01-18 13:41:00 +0800

    Construct player

--- Solitaire Final/Solitaire Final/src/application/controller/PlayerController.java
+++ Solitaire Final/Solitaire Final/src/application/controller/PlayerController.java
@@ -1,5 +1,26 @@
 package application.controller;
 
-public class PlayerController {
+import application.Main;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+public class PlayerController {
+	private Main main;
+	
+	@FXML
+	private TextField playerName;
+	
+	public void setMain(Main main)
+	{
+		this.main = main;
+	}
+	
+	@FXML
+	public void enter() {
+		//reference here yung player na initialized sa Main
+		//Main. = (playerName.getText().compareTo("") == 0)? "Player" : playerName.getText();
+		//dito naman yung calling ng method ng game from main
+		//main.
+	}
 }
