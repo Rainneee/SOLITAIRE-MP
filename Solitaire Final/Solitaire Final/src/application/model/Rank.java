1a7cb68c06271a00944213fb7b1c590345a55386
Romaine Dara Regala <rmregala@up.edu.ph>
2021-01-21 19:03:58 +0800

    Adjusted files

--- Solitaire Final/Solitaire Final/src/application/model/Rank.java
+++
@@ -1,9 +0,0 @@
-package application.model;
-/**
- * Enum representation of a card Rank
- * @author willn
- *
- */
-public enum Rank {
-	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
-}
